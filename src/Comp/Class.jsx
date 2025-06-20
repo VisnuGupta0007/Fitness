7f6fe19d861558fdce739dbbaa7bf3e250018c69
Visnu Gupta <vg8383654@gmail.com>
2025-06-20 10:36:51 +0530

    Update Class.jsx

--- src/Comp/Class.jsx
+++ src/Comp/Class.jsx
@@ -20,8 +20,7 @@ const center = {
     lng: -122.4194,
 };
 
-function Class() {
-
+function About() {
 
     // For Nav buttons
     const [activeLink, setActiveLink] = useState("HOME");
@@ -272,7 +271,7 @@ function Class() {
                             "Basic fitness focuses on simple exercises and healthy habits to improve strength, flexibility, and overall well-being."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                          <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -290,7 +289,7 @@ function Class() {
                             "Advanced Muscle Course helps you build <span className='text-blue-500'>strength, size</span>, and <span className='text-blue-500'>definition</span> with expert techniques."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                          <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -308,7 +307,7 @@ function Class() {
                             "New Gym Training guides beginners with effective workouts, proper form, and tips to build strength."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                           <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -326,7 +325,7 @@ function Class() {
                             "Yoga Training improves flexibility, balance, and mental clarity through poses, breath control, and relaxation."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                          <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -344,7 +343,7 @@ function Class() {
                             "Build strength and tone with beginner-friendly workouts and easy muscle-building tips."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                          <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -362,7 +361,7 @@ function Class() {
                             "Gain muscle, boost strength, and sculpt your body with expert bodybuilding workouts."
                         </p>
                         <button className="text-orange-600 font-semibold mt-2 hover:underline">
-                            Discover More
+                          <a href="/"> Discover More</a>
                         </button>
                     </div>
                 </div>
@@ -724,4 +723,4 @@ function Class() {
     )
 }
 
-export default Class
+export default About
